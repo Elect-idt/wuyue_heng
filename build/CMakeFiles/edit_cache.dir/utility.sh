25724dc595611c48a12de8a7ac8cc30faa36cd8e
jax_pan <353989733@qq.com>
2025-07-27 11:03:22 +0800

    忽略 build 目录

--- build/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /j/ArmsApprentice/wuyue_heng/0.reference/led_blink/build
-/usr/bin/ccmake.exe -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
--- build/cmake/stm32_stdperiph_driver/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /j/ArmsApprentice/wuyue_heng/0.reference/led_blink/build/cmake/stm32_stdperiph_driver
-/usr/bin/ccmake.exe -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
